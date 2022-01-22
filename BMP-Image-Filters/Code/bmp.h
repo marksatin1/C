c94fc316996bbc7e1feeb82b241a2b456334ed6f
Mark Satin <msatin2009@gmail.com>
2022-01-22 15:08:27 -0500

    Reformatted indents

--- BMP-Image-Filters/Code/bmp.h
+++ BMP-Image-Filters/Code/bmp.h
@@ -11,42 +11,39 @@ typedef uint16_t WORD;
 
 /* The BITMAPFILEHEADER structure contains information about the type, size, 
 and layout of a file that contains a DIB [device-independent bitmap]. */
-typedef struct
-{
-    WORD   bfType;
-    DWORD  bfSize;
-    WORD   bfReserved1;
-    WORD   bfReserved2;
-    DWORD  bfOffBits;
+typedef struct {
+  WORD   bfType;
+  DWORD  bfSize;
+  WORD   bfReserved1;
+  WORD   bfReserved2;
+  DWORD  bfOffBits;
 } __attribute__((__packed__))
 BITMAPFILEHEADER;
 
 
 /* The BITMAPINFOHEADER structure contains information about the 
 dimensions and color format of a DIB [device-independent bitmap]. */
-typedef struct
-{
-    DWORD  biSize;
-    LONG   biWidth;
-    LONG   biHeight;
-    WORD   biPlanes;
-    WORD   biBitCount;
-    DWORD  biCompression;
-    DWORD  biSizeImage;
-    LONG   biXPelsPerMeter;
-    LONG   biYPelsPerMeter;
-    DWORD  biClrUsed;
-    DWORD  biClrImportant;
+typedef struct {
+  DWORD  biSize;
+  LONG   biWidth;
+  LONG   biHeight;
+  WORD   biPlanes;
+  WORD   biBitCount;
+  DWORD  biCompression;
+  DWORD  biSizeImage;
+  LONG   biXPelsPerMeter;
+  LONG   biYPelsPerMeter;
+  DWORD  biClrUsed;
+  DWORD  biClrImportant;
 } __attribute__((__packed__))
 BITMAPINFOHEADER;
 
 
 /* RGBTRIPLE describes a color consisting of relative intensities 
 of red, green, and blue. */
-typedef struct
-{
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
+typedef struct {
+  BYTE  rgbtBlue;
+  BYTE  rgbtGreen;
+  BYTE  rgbtRed;
 } __attribute__((__packed__))
 RGBTRIPLE;
